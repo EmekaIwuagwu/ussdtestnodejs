42e48f896feeb2c4d7cfcad6096b05f3da41e11f
unknown <iwuagwuemmy@yahoo.com>
2021-10-20 15:34:16 +0100

    make it better

---
+++ server.js
@@ -0,0 +1,46 @@
+const app = require('express')()
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+
+const port = process.env.PORT || 3030
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.get('*', (req, res) => {
+  res.send('Welcome to Kigmoon TestBank')
+})
+
+app.post('*', (req,res) =>{
+
+ let {sessionId, serviceCode, phoneNum, text} = req.body;
+
+if(text == ''){
+	let response = `CON What would you like to check?
+	1. Get My Account
+	2. Get My Balance`
+	
+	res.send(response)
+	}
+	else if(text ==1){
+	let accNum = '0023391289'
+	let response = `Your Account number is ${accNum}`
+	
+	res.send(response)
+	}
+	
+	else if(text ==2){
+	let Balance = 5000000.00
+	let response = `Your Account Balance is ${Balance}`
+	
+	res.send(response)
+	}else{
+		res.status(400).send('Bad Request')
+	}
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
